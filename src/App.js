82b230040ea06ff76c5099eccf6d065a046c2a90
tim <tim@tims-MacBook-Air.local>
2019-05-12 03:16:38 -0400

    CM #2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import HeaderNav from './containers/HeaderNav/HeaderNav';
+import {SideBar} from './containers/SideBar/SideBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+        <HeaderNav/>
+        <SideBar/>
+      </React.Fragment>
+    // {/*<div className="App">*/}
+    //   {/*<header className="App-header">*/}
+    //     {/*<img src={logo} className="App-logo" alt="logo" />*/}
+    //     {/*<p>*/}
+    //       {/*Edit <code>src/App.js</code> and save to reload.*/}
+    //     {/*</p>*/}
+    //     {/*<a*/}
+    //       {/*className="App-link"*/}
+    //       {/*href="https://reactjs.org"*/}
+    //       {/*target="_blank"*/}
+    //       {/*rel="noopener noreferrer"*/}
+    //     {/*>*/}
+    //       {/*Learn React*/}
+    //     {/*</a>*/}
+    //   {/*</header>*/}
+    // {/*</div>*/}
   );
 }
 
